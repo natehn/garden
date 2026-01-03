88ce4e367cc76cf7302db735ffd6fbc6feafa79a
nathan h <66496322+natehn@users.noreply.github.com>
2026-01-03 02:06:26 -0500

    Changing url

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "garden 🌱",
+    pageTitle: "fragments 🌱",
     pageTitleSuffix: " - nathan hewitt",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "garden.hew.tt",
+    baseUrl: "fragments.hew.tt",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
